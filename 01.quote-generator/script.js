dcc37ff913095fc922aa4d52a9468d383bb3146f
DalatCoder <hieuntctk42@gmail.com>
2020-08-13 12:00:30 +0700

    01.Fetch quote and populate data

--- 01.quote-generator/script.js
+++ 01.quote-generator/script.js
@@ -1,13 +1,28 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote From API
 async function getQuote() {
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const proxyUrl = 'https://dalatcoder-proxy-server.herokuapp.com/';
   const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+
+    authorText.innerText = data.quoteAuthor || 'Unknown';
+
+    // Reducre font size for long quote
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     getQuote();
     console.log('whoops, no quote', error);
